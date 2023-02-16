4768a6c98c748f6c3ff503710cc06fc668b23b67
lalankumar001 <tocsi.org@gmail.com>
2023-02-16 16:57:43 +0530

    InputFieldAdded

--- src/Components/Meals/MealsItem/MealitemForum.js
+++ src/Components/Meals/MealsItem/MealitemForum.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import styles from './MealitemForum.module.css'
+import React from "react";
+import Input from "../../UI/Input";
+import styles from "./MealitemForum.module.css";
 
 const MealitemForum = () => {
   return (
-    <div>MealitemForum</div>
-  )
-}
+    <div>
+      <form className={styles.form}>
+        {/* input yaha input component se aa raha h jo ki ui file m h */}
+        <Input
+          label="Amount"
+          input={{
+            id: 'amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1',
+          }}
+        />
+        <button>+ Add</button>
+      </form>
+    </div>
+  );
+};
 
-export default MealitemForum
+export default MealitemForum;
